917ffb222b4548e11330944854fccf4c232f6f0a
Vitor Mourão <46322896+VitorMourao@users.noreply.github.com>
2019-09-27 10:05:43 -0300

    Changed the complexity of the code.
    Changed to function.
    Added looping where needed.

--- EmptyBook.R
+++ EmptyBook.R
@@ -1,27 +1,29 @@
-library(openxlsx) # import the openxlsx library
+#A function to create a xlsx document.
+xlsxTemplate <- function(file){
+  wb <- openxlsx::createWorkbook() # create the workbook
 
-wb <- createWorkbook() # create the workbook (xlsx archive that contains the sheets)
+  # Sheet names
+  sheetNames <- list("st","ct","par","inp","sw","sim")
 
-# Add every sheet we want, in order, to our workbook
-addWorksheet(wb, sheetName = "st", gridLines = TRUE)
-addWorksheet(wb, sheetName = "ct", gridLines = TRUE)
-addWorksheet(wb, sheetName = "par", gridLines = TRUE)
-addWorksheet(wb, sheetName = "inp", gridLines = TRUE)
-addWorksheet(wb, sheetName = "sw", gridLines = TRUE)
-addWorksheet(wb, sheetName = "sim", gridLines = TRUE)
+  for (x in sheetNames){
+    openxlsx::addWorksheet(wb, sheetName = x, gridLines = TRUE)
+  }
 
-# Create the names of our columns (if we use c() instead of list() we have the row names)
-colnames1 <- list("Variable", "Value", "Unit", "Description")
-colnames2 <- list("Variable", "Value", "Unit", "Description", "Interpolation")
+  # Columns
+  colNames <- list("Variable", "Value", "Unit", "Description")
+  colNamesInp <- list("Variable", "Value", "Unit", "Description", "Interpolation")
 
-# Write the names in our archive starting from column 1 and row 1
-# x = list of names that our columns will have.
-writeData(wb, 1, x = colnames1,startCol = 1, startRow = 1, colNames = TRUE)
-writeData(wb, 2, x = colnames1,startCol = 1, startRow = 1, colNames = TRUE)
-writeData(wb, 3, x = colnames1,startCol = 1, startRow = 1, colNames = TRUE)
-writeData(wb, 4, x = colnames2,startCol = 1, startRow = 1, colNames = TRUE)
-writeData(wb, 5, x = colnames1,startCol = 1, startRow = 1, colNames = TRUE)
-writeData(wb, 6, x = colnames1,startCol = 1, startRow = 1, colNames = TRUE)
-
-# Save the workbook
-saveWorkbook(wb, "emptyBook.xlsx", overwrite = TRUE)
+  # Write data in the sheets
+  for(i in 1:lenght(sheetNames)){
+    if(sheetNames[i] != "inp"){
+      openxlsx::writeData(wb, i, x = colNames,
+                          startCol = 1, startRow = 1, colNames = TRUE)
+    }
+    else{
+      openxlsx::writeData(wb, i, x = colNamesInp,
+                          startCol = 1, startRow = 1, colNames = TRUE)
+    }
+  }
+  
+  # Save the workbook
+  openxlsx::saveWorkbook(wb, file, overwrite = TRUE)
